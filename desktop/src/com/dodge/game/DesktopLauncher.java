aa29f78c50b1027d340dc354c1c5a09f8b869cc4
Caleb-Hurst <hursty.caleb@gmail.com>
2024-02-10 14:07:12 -0600

    Initial push

---
+++ desktop/src/com/dodge/game/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.dodge.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.dodge.game.DodgeGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Dodge");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new DodgeGame(), config);
+	}
+}
